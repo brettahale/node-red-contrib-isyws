9606df32d60ed0766ed60bf3e095409a74c12894
brett.hale <brett.hale@ticketmaster.com>
2020-05-02 21:41:03 -0600

    init

--- isy-node.js
+++ isy-node.js
@@ -1,41 +1,46 @@
+module.exports = function(RED) {
 
+    const WebSocket = require('ws');
+    const parseString = require('xml2js').parseString;
 
-class WsConnector{
-
-    constructor(node, config) {
-        this.node = node;
-        this.config = config;
-        this.reconnectInterval = 10*1000;
-        this.connect = this.setup_connect();
-    }
-
-    close(){
-
-    }
-
-    setup_connect(){
-        let self = this;
-        let node = self.node;
-        let config = self.config;
+    function WebSocketISY(config) {
+        let node = this;
+        RED.nodes.createNode(node, config);
+        node.log('New WebSocketISY');
         let auth = 'Basic ' + Buffer.from(node.credentials.username + ':' + node.credentials.password).toString('base64');
         let url = `${config.ssl?'wss':'ws'}://${config.host}:${config.port}/rest/subscribe/`;
+        let reconnectInterval = 10*1000;
 
-        return function(){
-            self.node.log('connecting to isy');
+        function connect(){
+            node.log(`connecting to isy: ${url}`);
             node.ws = new WebSocket(url,
-                                   'ISYSUB',
-                                   {
-                                       origin: 'com.universal-devices.websockets.isy',
-                                       headers: {Authorization: auth}
-                                   });
+                                    'ISYSUB',
+                                    {
+                                        origin: 'com.universal-devices.websockets.isy',
+                                        headers: {Authorization: auth}
+                                    });
+
+            node.ws.on('error', function(err) {
+                node.status({fill:"red",shape:"ring",text:"disconnected"});
+                node.error(`Socket Error: ${err}`);
+            });
 
             node.ws.on('message', function(message){
-                node.log(message);
                 parseString(message, function (err, result) {
                     if (err) {
                         node.error(err, message);
+                    } else if (result.SubscriptionResponse) {
+                        node.log('subscribed');
                     } else {
-                        node.send({payload:result});
+                        node.log(JSON.stringify(result));
+
+                        isyEvent = {
+                            control: result.Event.control[0],
+                            action: result.Event.action[0],
+                            eventMeta: result.Event.eventInfo[0],
+                            rawEvent: result
+                        };
+                        node.send({payload:isyEvent});
                     }
                 });
             });
@@ -45,52 +50,22 @@ class WsConnector{
                 node.status({fill:"green",shape:"dot",text:"connected"});
             });
 
-            node.ws.on('error', function(err) {
-                node.status({fill:"red",shape:"ring",text:"disconnected"});
-                node.error(err);
-            });
-
             node.ws.on('close', function(err) {
-                node.status({fill:"red",shape:"ring",text:"disconnected"});
-                node.log('connection closed, reconnecting');
-                node.error(err);
-                node.ws_timeout = setTimeout(self.connect, self.reconnectInterval);
+                node.error(`Socket Closed: ${err}`);
+                node.log('reconnecting...');
+                node.ws_timeout = setTimeout(connect, reconnectInterval);
             });
-        };
-
-    };
-
-}
-
-module.exports = function(RED) {
-
-    const WebSocket = require('ws');
-    const parseString = require('xml2js').parseString;
-
-    function WebSocketISY(config) {
-        let node = this;
-        RED.nodes.createNode(node, config);
-
-        function connect(){
-
         }
 
-        node.on('opened', function(event) {
-            node.status({fill:"green",shape:"dot",text: "connected"});
-        });
-
-        node.on('error', function(event) {
-            node.status({fill:"red",shape:"ring",text:"error"});
-        });
-
-        node.on('closed', function(event) {
+        node.on('close', function(){
+            node.log('node close');
             node.status({fill:"red",shape:"ring",text:"common.status.disconnected"});
+            node.ws.terminate();
+            clearTimeout(node.ws_timeout);
+            node.ws_timeout = null;
         });
 
-        this.on('close', function(){
-           clearTimeout(self.ws_timeout);
-           node.ws_timeout = null;
-        });
+        connect();
     }
 
     RED.nodes.registerType("isy-ws", WebSocketISY, {
